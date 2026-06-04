a69f0b60c48eddfab5fb0b5a2c576a28ef2347f7
Ankushh0027 <ankush270008@gmail.com>
2026-06-04 13:48:30 +0530

    JS basics

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,29 @@
+"use strict";//Treat all JS code as newer version
+
+//alert(3 + 3);//we are using nodeJS,not browser, so alert is not defined.
+
+console.log(3
+     + 3);//code readability should be high
+
+console.log("Ankush" );
+
+let name = "Ankush"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined);//Undefined
+console.log(typeof null);//object
